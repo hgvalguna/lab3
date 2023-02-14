25389127f53388572c1587a9b3564905542b2080
hgvalguna <hgvalguna@student.apc.edu.ph>
2023-02-14 15:25:43 +0800

    update

---
+++ ci4/app/Views/pages/home.php
@@ -0,0 +1,1 @@
+Welcome to my Home Page! Henzon Ivan V
---
+++ ci4/app/Views/templates/header.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+<head>
+<?php echo link_tag('css/style.css'); 
+echo script_tag('js/main.js'); ?>
+    <title>CodeIgniter Tutorial</title>
+</head>
+<body>
+
+    <h1><?= esc($title) ?></h1>
