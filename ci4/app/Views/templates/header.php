a644442baa81d6380bbcc813ce8fe0ab204dc3ab
hgvalguna <hgvalguna@student.apc.edu.ph>
2023-02-14 18:21:36 +0800

    update

--- ci4/app/Views/templates/header.php
+++ ci4/app/Views/templates/header.php
@@ -1,8 +1,8 @@
 <!doctype html>
 <html>
 <head>
-<?php echo link_tag('css/style.css'); 
-echo script_tag('js/main.js'); ?>
+<?php echo link_tag('assets/css/style.css'); 
+echo script_tag('assets/js/main.js'); ?>
     <title>CodeIgniter Tutorial</title>
 </head>
 <body>
