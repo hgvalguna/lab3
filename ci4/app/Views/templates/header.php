e354b1fb3066a270be4462698cc55a7dcbfbe8c6
hgvalguna <hgvalguna@student.apc.edu.ph>
2023-02-14 18:38:02 +0800

    update

--- ci4/app/Views/templates/header.php
+++ ci4/app/Views/templates/header.php
@@ -1,8 +1,20 @@
 <!doctype html>
 <html>
 <head>
-<?php echo link_tag('assets/css/style.css'); 
-echo script_tag('assets/js/main.js'); ?>
+<?php echo link_tag('assets/css/style.css');
+echo link_tag('assets/vendor/bootstrap/css/bootstrap.min.css');
+echo link_tag('assets/vendor/bootstrap-icons/bootstrap-icons.css');
+echo link_tag('assets/vendor/boxicons/css/boxicons.min.css');
+echo link_tag('assets/vendor/glightbox/css/glightbox.min.css');
+echo link_tag('assets/vendor/remixicon/remixicon.css');
+echo link_tag('assets/vendor/swiper/swiper-bundle.min.css');
+echo script_tag('assets/vendor/purecounter/purecounter_vanilla.js');
+echo script_tag('assets/vendor/bootstrap/js/bootstrap.bundle.min.js');
+echo script_tag('assets/vendor/glightbox/js/glightbox.min.js');
+echo script_tag('assets/vendor/isotope-layout/isotope.pkgd.min.js');
+echo script_tag('assets/vendor/swiper/swiper-bundle.min.js');
+echo script_tag('assets/vendor/waypoints/noframework.waypoints.js');
+echo script_tag('assets/vendor/php-email-form/validate.js'); ?>
     <title>CodeIgniter Tutorial</title>
 </head>
 <body>
