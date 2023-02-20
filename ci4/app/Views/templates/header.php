7a2263a55b97432775c15e9811b9c84fd219cc48
hgvalguna <hgvalguna@student.apc.edu.ph>
2023-02-20 10:26:08 +0800

    Update header.php

--- ci4/app/Views/templates/header.php
+++ ci4/app/Views/templates/header.php
@@ -15,7 +15,7 @@ echo script_tag('assets/vendor/isotope-layout/isotope.pkgd.min.js');
 echo script_tag('assets/vendor/swiper/swiper-bundle.min.js');
 echo script_tag('assets/vendor/waypoints/noframework.waypoints.js');
 echo script_tag('assets/vendor/php-email-form/validate.js');
-echo script_tag('assets/js/main.js') ?>
+echo script_tag('assets/js/main.js'); ?>
     <title>CodeIgniter Tutorial</title>
 </head>
 <body>
