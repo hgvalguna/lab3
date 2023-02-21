91abc821a8ae08a41b4963cfeafffadf726a3dc0
hgvalguna <hgvalguna@student.apc.edu.ph>
2023-02-21 14:04:15 +0800

    added files

--- ci4/app/Views/pages/home.php
+++ ci4/app/Views/pages/home.php
@@ -17,12 +17,12 @@
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="<?= base_url('assets/vendor/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet">
-  <link href="<?= base_url('assets/vendor/bootstrap-icons/bootstrap-icons.css'); ?>" rel="stylesheet">
-  <link href="<?= base_url('assets/vendor/boxicons/css/boxicons.min.css'); ?>" rel="stylesheet">
-  <link href="<?= base_url('assets/vendor/glightbox/css/glightbox.min.css'); ?>" rel="stylesheet">
-  <link href="<?= base_url('assets/vendor/remixicon/remixicon.css');?>" rel="stylesheet">
-  <link href="<?= base_url('assets/vendor/swiper/swiper-bundle.min.css');?>" rel="stylesheet"> 
+  <link href="<?= base_url('vendor/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet">
+  <link href="<?= base_url('vendor/bootstrap-icons/bootstrap-icons.css'); ?>" rel="stylesheet">
+  <link href="<?= base_url('vendor/boxicons/css/boxicons.min.css'); ?>" rel="stylesheet">
+  <link href="<?= base_url('vendor/glightbox/css/glightbox.min.css'); ?>" rel="stylesheet">
+  <link href="<?= base_url('vendor/remixicon/remixicon.css');?>" rel="stylesheet">
+  <link href="<?= base_url('vendor/swiper/swiper-bundle.min.css');?>" rel="stylesheet"> 
 
   <!-- Template Main CSS File -->
   <link href="<?=base_url('assets/css/style.css'); ?>" rel="stylesheet"> 
@@ -391,13 +391,13 @@
 
 
   <!-- Vendor JS Files -->
-  <script src="<?= base_url('assets/vendor/purecounter/purecounter_vanilla.js'); ?>"></script>
-  <script src="<?= base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
-  <script src="<?= base_url('assets/vendor/glightbox/js/glightbox.min.js'); ?>"></script>
-  <script src="<?= base_url('assets/vendor/isotope-layout/isotope.pkgd.min.js'); ?>"></script>
-  <script src="<?= base_url('assets/vendor/swiper/swiper-bundle.min.js'); ?>"></script>
-  <script src="<?= base_url('assets/vendor/waypoints/noframework.waypoints.js'); ?>"></script>
-  <script src="<?= base_url('assets/vendor/php-email-form/validate.js'); ?>"></script> 
+  <script src="<?= base_url('vendor/purecounter/purecounter_vanilla.js'); ?>"></script>
+  <script src="<?= base_url('vendor/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
+  <script src="<?= base_url('vendor/glightbox/js/glightbox.min.js'); ?>"></script>
+  <script src="<?= base_url('vendor/isotope-layout/isotope.pkgd.min.js'); ?>"></script>
+  <script src="<?= base_url('vendor/swiper/swiper-bundle.min.js'); ?>"></script>
+  <script src="<?= base_url('vendor/waypoints/noframework.waypoints.js'); ?>"></script>
+  <script src="<?= base_url('vendor/php-email-form/validate.js'); ?>"></script> 
 
   <!-- Template Main JS File -->
   <script src="<?= base_url('assets/js/main.js');?>"></script> 
