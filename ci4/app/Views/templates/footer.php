572df66e50a4315430963b4cebb757e5bed71493
hgvalguna <hgvalguna@student.apc.edu.ph>
2023-02-21 15:45:46 +0800

    update

--- ci4/app/Views/templates/footer.php
+++ ci4/app/Views/templates/footer.php
@@ -1,4 +1,11 @@
 <em>&copy; 2023</em>
-<?php echo script_tag('assets/js/main.js'); ?>
+<?php echo script_tag('assets/js/main.js');
+echo script_tag('assets/purecounter/purecounter_vanilla.js');
+echo script_tag('assets/bootstrap/js/bootstrap.bundle.min.js');
+echo script_tag('assets/glightbox/js/glightbox.min.js');
+echo script_tag('assets/isotope-layout/isotope.pkgd.min.js');
+echo script_tag('assets/swiper/swiper-bundle.min.js');
+echo script_tag('assets/waypoints/noframework.waypoints.js');
+echo script_tag('assets/php-email-form/validate.js'); ?>
 </body>
 </html>
--- ci4/app/Views/templates/header.php
+++ ci4/app/Views/templates/header.php
@@ -1,8 +1,26 @@
 <!doctype html>
 <html>
 <head>
-<?php echo link_tag('assets/css/style.css'); ?>
-    <title>CodeIgniter Tutorial</title>
+<meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>SIUUU</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.ico" rel="icon" type="images/favicon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+<?php echo link_tag('assets/css/style.css');
+echo link_tag('assets/bootstrap/css/bootstrap.min.css');
+echo link_tag('assets/bootstrap-icons/bootstrap-icons.css');
+echo link_tag('assets/boxicons/css/boxicons.min.css');
+echo link_tag('assets/glightbox/css/glightbox.min.css');
+echo link_tag('assets/remixicon/remixicon.css');
+echo link_tag('assets/swiper/swiper-bundle.min.css'); ?>
 </head>
 <body>
 
