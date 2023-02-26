5d09ada2e915c3530de2ef39bc1e6243586261b0
hgvalguna <hgvalguna@student.apc.edu.ph>
2023-02-26 17:46:27 +0800

    fix image

--- ci4/app/Views/templates/footer.php
+++ ci4/app/Views/templates/footer.php
@@ -1,5 +1,5 @@
 <em>&copy; 2023</em>
-<!-- Vendor JS Files -->
+<!-- Assets JS Files -->
 <script src="<?= base_url('assets/purecounter/purecounter_vanilla.js'); ?>"></script>
 <script src="<?= base_url('assets/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
 <script src="<?= base_url('assets/glightbox/js/glightbox.min.js'); ?>"></script>
@@ -7,7 +7,8 @@
 <script src="<?= base_url('assets/swiper/swiper-bundle.min.js'); ?>"></script>
 <script src="<?= base_url('assets/waypoints/noframework.waypoints.js'); ?>"></script>
 <script src="<?= base_url('assets/php-email-form/validate.js'); ?>"></script> 
-<!-- Template Main JS File -->
-<script src="<?= base_url('assets/js/main.js');?>"></script> 
+<!-- Template Main JS File 
+<script src="<?= base_url('assets/js/main.js');?>"></script> -->
+<?php script_tag('assets/js/main.js'); ?>
 </body>
 </html>
