0c3a51fe06dfc12b21b4c2885b58826865843c3a
hgvalguna <hgvalguna@student.apc.edu.ph>
2023-02-27 17:44:33 +0800

    added guest form

--- ci4/app/Views/pages/home.php
+++ ci4/app/Views/pages/home.php
@@ -46,6 +46,7 @@
           <li><a class="nav-link" href="#about">About</a></li>
           <li><a class="nav-link" href="#portfolio">Images</a></li>
           <li><a class="nav-link" href="#reference">References</a></li>
+          <li><a class="nav-link" href="guests">Guests' List</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
