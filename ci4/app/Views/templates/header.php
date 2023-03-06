7f1c99f91bfd96daef170c5352f735c9d00258e2
hgvalguna <hgvalguna@student.apc.edu.ph>
2023-03-06 09:19:59 +0800

    Update header.php

--- ci4/app/Views/templates/header.php
+++ ci4/app/Views/templates/header.php
@@ -43,7 +43,7 @@ echo link_tag('assets/css/style.css'); ?>
 <header id="header">
     <div class="container">
 
-      <h1><a href="home">Ivan Valguna</a></h1>
+      <h1><a href="/lab3/ci4/public/home">Ivan Valguna</a></h1>
       <!-- Uncomment below if you prefer to use an image logo 
       <a href="home" class="mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a> -->
       <h2>I'm a <span>Computer Science Student</span></h2>
