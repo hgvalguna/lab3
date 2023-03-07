c3e4c9ec4b21bd9441ce0ecd18b2c272b1fa11e8
hgvalguna <hgvalguna@student.apc.edu.ph>
2023-03-07 08:43:41 +0800

    Update footer.php

--- ci4/app/Views/templates/footer.php
+++ ci4/app/Views/templates/footer.php
@@ -1,3 +1,23 @@
+<!-- Assets JS Files 
+<script src="<?= base_url('assets/purecounter/purecounter_vanilla.js'); ?>"></script>
+<script src="<?= base_url('assets/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
+<script src="<?= base_url('assets/glightbox/js/glightbox.min.js'); ?>"></script>
+<script src="<?= base_url('assets/isotope-layout/isotope.pkgd.min.js'); ?>"></script>
+<script src="<?= base_url('assets/swiper/swiper-bundle.min.js'); ?>"></script>
+<script src="<?= base_url('assets/waypoints/noframework.waypoints.js'); ?>"></script>
+<script src="<?= base_url('assets/php-email-form/validate.js'); ?>"></script> -->
+
+<?php script_tag('assets/purecounter/purecounter_vanilla.js'); ?>
+<?php script_tag('assets/bootstrap/js/bootstrap.bundle.min.js'); ?>
+<?php script_tag('assets/glightbox/js/glightbox.min.js'); ?>
+<?php script_tag('assets/isotope-layout/isotope.pkgd.min.js'); ?>
+<?php script_tag('assets/swiper/swiper-bundle.min.js'); ?>
+<?php script_tag('assets/waypoints/noframework.waypoints.js'); ?>
+<?php script_tag('assets/php-email-form/validate.js'); ?>
+
+<!-- Template Main JS File 
+<script src="<?= base_url('assets/js/main.js');?>"></script> -->
+<?php script_tag('assets/js/main.js'); ?>
 <em>&copy; 2023</em>
 </body>
 </html>
