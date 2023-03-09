289128bcc0689992a3c83c67a48e7d4db79c0567
hgvalguna <hgvalguna@student.apc.edu.ph>
2023-03-09 12:20:56 +0800

    Update header.php

--- ci4/app/Views/templates/header.php
+++ ci4/app/Views/templates/header.php
@@ -22,22 +22,13 @@
   <link href="<?= base_url('assets/glightbox/css/glightbox.min.css'); ?>" rel="stylesheet">
   <link href="<?= base_url('assets/remixicon/remixicon.css');?>" rel="stylesheet">
   <link href="<?= base_url('assets/swiper/swiper-bundle.min.css');?>" rel="stylesheet">
-  <!-- Assets JS Files -->
-<script src="<?= base_url('assets/purecounter/purecounter_vanilla.js'); ?>"></script>
-<script src="<?= base_url('assets/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
-<script src="<?= base_url('assets/glightbox/js/glightbox.min.js'); ?>"></script>
-<script src="<?= base_url('assets/isotope-layout/isotope.pkgd.min.js'); ?>"></script>
-<script src="<?= base_url('assets/swiper/swiper-bundle.min.js'); ?>"></script>
-<script src="<?= base_url('assets/waypoints/noframework.waypoints.js'); ?>"></script>
-<script src="<?= base_url('assets/php-email-form/validate.js'); ?>"></script> 
+  
 
   <!-- Template Main CSS File 
   <link href="<?=base_url('assets/css/style.css'); ?>" rel="stylesheet"> -->
 <?php
 echo link_tag('assets/css/style.css'); ?>
-<!-- Template Main JS File 
-<script src="<?= base_url('assets/js/main.js');?>"></script> -->
-<?php script_tag('assets/js/main.js'); ?>
+
 </head>
 <body>
 <header id="header">
